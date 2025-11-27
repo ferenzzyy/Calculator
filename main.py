25b7f70e76af2758c05f35f0215afe29c2f54f79
Ferenzzyyy <fernandesranen05@gmail.com>
2025-11-27 17:02:56 +0000

    Butttons!!!
    Created an easier way to make new buttons using classes and dictionaries. No need to create a new class over and over just add to the button data dictionary

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import pygame
 
 # pygame setup
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+screen = pygame.display.set_mode((700, 500))
 clock = pygame.time.Clock()
 running = True
 dt = 0
+font = pygame.font.SysFont('Arial',40)
 
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
@@ -21,16 +22,38 @@ class Button():
         self.button_rect = pygame.Rect((640,360),(self.size.x, self.size.y))
 
     def DrawButton(self):
-        self.button_rect.centerx = self.position.x
-        self.button_rect.centery = self.position.y
+        self.button_rect.center = self.position
         pygame.draw.rect(screen, self.colour, self.button_rect)
+        btn_txt_surface = font.render(self.btn_txt, True, "black")
+        btn_txt_rect = btn_txt_surface.get_rect()
+        btn_txt_rect.center = self.position
+        screen.blit(btn_txt_surface, btn_txt_rect)
 
     def CheckForMouse(self):
-        return self.button_rect.collidepoint(mouse_pos.x, mouse_pos.y )
+        return self.button_rect.collidepoint(mouse_pos.x, mouse_pos.y)
+
+    def Clicking(self):
+            if pygame.mouse.get_just_pressed()[0] and self.CheckForMouse():
+                print(f"{self.btn_txt} Clicked")
+        
+button_data = [
+    {"number" : "1", "position" : pygame.Vector2(55, 200)},
+    {"number" : "2", "position" : pygame.Vector2(160, 200)},
+    {"number" : "3", "position" : pygame.Vector2(265, 200)},
+    {"number" : "4", "position" : pygame.Vector2(55, 255)},
+    {"number" : "5", "position" : pygame.Vector2(160, 255)},
+    {"number" : "4", "position" : pygame.Vector2(265, 255)}
+]
+
+# Creates a list of buttons for the numbers 
+# uses the values given from the data 
+# for each piece of data it creates a new button class
+number_btns = [Button(data["number"], "white", 100, 50, data["position"].x, data["position"].y) for data in button_data]
 
 
 
-test_btn = Button("Test", "blue", 100.0, 50, pos.x, pos.y)
+
+
 
 while running:
     mouse_pos = pygame.Vector2(pygame.mouse.get_pos(desktop=False)[0], pygame.mouse.get_pos(desktop=False)[1])
@@ -39,29 +62,30 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        if event.type == pygame.MOUSEBUTTONDOWN and test_btn.CheckForMouse():
-            print("Button Clicked!")
+        # if event.type == pygame.MOUSEBUTTONDOWN and number1_btn.CheckForMouse():
+        #     print(number1_btn.btn_txt + ' Clicked!')
 
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
-    # print()
+    screen.fill("black")
+
+    for item in number_btns:
+        item.DrawButton()
+        item.Clicking()
     
 
-    
-    test_btn.DrawButton()
-    # print(test_btn.button_rect.center)
 
-    pygame.draw.circle(screen, "red", player_pos, 40)
+   
+    # pygame.draw.circle(screen, "red", player_pos, 40)
 
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-        player_pos.y -= 300 * dt
-    if keys[pygame.K_s]:
-        player_pos.y += 300 * dt
-    if keys[pygame.K_a]:
-        player_pos.x -= 300 * dt
-    if keys[pygame.K_d]:
-        player_pos.x += 300 * dt
+    # keys = pygame.key.get_pressed()
+    # if keys[pygame.K_w]:
+    #     player_pos.y -= 300 * dt
+    # if keys[pygame.K_s]:
+    #     player_pos.y += 300 * dt
+    # if keys[pygame.K_a]:
+    #     player_pos.x -= 300 * dt
+    # if keys[pygame.K_d]:
+    #     player_pos.x += 300 * dt
 
     # print(player_pos)
 
