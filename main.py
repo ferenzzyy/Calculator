78129ce1c87a1c7b4a3e129594968bb8a760cbdf
Ferenzzyyy <fernandesranen05@gmail.com>
2025-11-27 16:03:09 +0000

    Created basic Scene
    Basic Pygame Scene, and a basic test button

--- main.py
+++ main.py
@@ -10,15 +10,46 @@ dt = 0
 
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
+pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+
+class Button():
+    def __init__(self, btn_text, colour, size_x, size_y, pos_x, pos_y):
+        self.btn_txt = btn_text
+        self.size = pygame.math.Vector2(size_x, size_y)
+        self.position = pygame.math.Vector2(pos_x, pos_y)
+        self.colour = colour
+        self.button_rect = pygame.Rect((640,360),(self.size.x, self.size.y))
+
+    def DrawButton(self):
+        self.button_rect.centerx = self.position.x
+        self.button_rect.centery = self.position.y
+        pygame.draw.rect(screen, self.colour, self.button_rect)
+
+    def CheckForMouse(self):
+        return self.button_rect.collidepoint(mouse_pos.x, mouse_pos.y )
+
+
+
+test_btn = Button("Test", "blue", 100.0, 50, pos.x, pos.y)
+
 while running:
+    mouse_pos = pygame.Vector2(pygame.mouse.get_pos(desktop=False)[0], pygame.mouse.get_pos(desktop=False)[1])
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.MOUSEBUTTONDOWN and test_btn.CheckForMouse():
+            print("Button Clicked!")
 
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("purple")
+    # print()
+    
+
+    
+    test_btn.DrawButton()
+    # print(test_btn.button_rect.center)
 
     pygame.draw.circle(screen, "red", player_pos, 40)
 
@@ -31,6 +62,8 @@ while running:
         player_pos.x -= 300 * dt
     if keys[pygame.K_d]:
         player_pos.x += 300 * dt
+
+    # print(player_pos)
 
     # flip() the display to put your work on screen
     pygame.display.flip()
